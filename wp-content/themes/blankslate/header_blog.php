21c0dbf9469bb452008636b07b13055ec291ac47
Jonathan Calderon Roa <Jomi@MacBook-Pro-de-Jonathan.local>
2016-06-14 08:40:32 -0500

    change background color to  tickets item in menu

--- wp-content/themes/blankslate/header_blog.php
+++ wp-content/themes/blankslate/header_blog.php
@@ -24,7 +24,7 @@
 				<li><a href="/blog">Blog</a></li>
 				<li><a href="#schedule">Schedule</a></li>
 				<li><a href="#speakers">Speakers</a></li>
-				<li><a href="https://www.eventbrite.com/e/work-awesome-tickets-21453749743" target="_blank">Tickets</a></li>
+				<li class="ticket"><a href="https://www.eventbrite.com/e/work-awesome-tickets-21453749743" target="_blank">Tickets</a></li>
 				<li><a href="mailto:​felix@workawesome.org,lars@workawesome.org?​subject=Work%20Awesome%20-%20I'd%20like%20to%20be%20involved">Contact</a></li>
 			</ul>
 		</div>
