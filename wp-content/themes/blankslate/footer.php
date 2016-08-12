05e4cf9c7ad662ac6cfcdeb5ce628fd3b9c9db0d
Mercy Roa Vargas <mercypatin@gmail.com>
2016-08-12 23:13:37 +0000

    change templates in blog, both sites

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -1,23 +1,30 @@
-<?php extract((array)json_decode(get_option('awesome_settings') ) ); ?>
-<div class="">
-	<section id="footer">
-		<p><?php echo stripslashes($Footer->description); ?></p>
-		<!--<p class="copy">&copy;2016 Inbox Awesome. all rights reserved</p>-->
-		<div class="socialnetworks">
-			<div class="cell"><a href="#" class="facebook"></a></div>
-			<div class="cell"><a href="#" class="twitter"></a></div>
-			<div class="cell"><a href="#" class="instagram"></a></div>
+			<?php extract((array)json_decode(get_option('awesome_settings') ) ); ?>
+
+				</div>
+				<div id="sidebar" class="sidebar col-sm-4">
+					<?php get_sidebar(); ?>
+				</div><!-- .sidebar -->
+			</div>
+			<div class="">
+				<section id="footer">
+					<p><?php echo stripslashes($Footer->description); ?></p>
+					<!--<p class="copy">&copy;2016 Inbox Awesome. all rights reserved</p>-->
+					<div class="socialnetworks">
+						<div class="cell"><a href="#" class="facebook"></a></div>
+						<div class="cell"><a href="#" class="twitter"></a></div>
+						<div class="cell"><a href="#" class="instagram"></a></div>
+					</div>
+				</section>
+			</div>
+			</div>
 		</div>
-	</section>
-</div>
-	</div>
-<?php  wp_footer(); ?>
-<script>
-	jQuery(document).ready(function(){
-		if( jQuery('#wpadminbar').size() > 0 ){
-			jQuery('#head_menu').css('top',jQuery('#wpadminbar').height() );
-		}
-	});
-</script>
-</body>
+		<?php  wp_footer(); ?>
+		<script>
+			jQuery(document).ready(function(){
+				if( jQuery('#wpadminbar').size() > 0 ){
+					jQuery('#head_menu').css('top',jQuery('#wpadminbar').height() );
+				}
+			});
+		</script>
+	</body>
 </html>
--- wp-content/themes/blankslate/header.php
+++ wp-content/themes/blankslate/header.php
@@ -6,6 +6,7 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri (); ?>/blog.css" />
 <?php wp_head(); ?>
 <link href='<?php echo get_template_directory_uri(); ?>/js/jquery.bxslider/jquery.bxslider.css' rel='stylesheet' type='text/css'>
@@ -13,25 +14,46 @@
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
 <header id="header" role="banner">
-<!-- <nav id="menu" role="navigation">
-<?php //wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>  -->
 </header>
 	<div id="head_menu">
 		<div class="menu_contain">
 			<ul id="menu">
-				<li><a href="/#home">Home</a></li>
+				<li><a href="#home">Home</a></li>
 				<li><a href="/blog">Blog</a></li>
-				<li><a href="/#schedule">Schedule</a></li>
-				<li><a href="/#speakers">Speakers</a></li>
+				<li><a href="#schedule">Schedule</a></li>
+				<li><a href="#speakers">Speakers</a></li>
 				<li class="ticket"><a href="https://www.eventbrite.com/e/work-awesome-tickets-21453749743" target="_blank">Tickets</a></li>
 				<li><a href="mailto:​felix@workawesome.org,lars@workawesome.org?​subject=Work%20Awesome%20-%20I'd%20like%20to%20be%20involved">Contact</a></li>
 			</ul>
 		</div>
 	</div>
 <div id="container">
-	<section id="home">
+	<div class="container">
+		<div class="bg-main"></div>
+		<div class="fade-layer"></div>
+		<section id="home">
+			<div class="table_cell">
+				<div class="description">
+		<?php //extract((array)json_decode(get_option('awesome_settings') ) ); ?>
+					<p class="subtitle">
+						<?php //echo stripslashes($Home->date); ?>
+						</p>
+					<h1 style="display:none;">Work Awesome</h1>
+					<img style="width:100%;" src="<?php echo get_template_directory_uri (); ?>/img/logo2.png" alt="workawesome" style="">
+					<div class="">
+					<p class="subtitle">
+						The Work Awesome Blog
+					</p>
+				</div>
+					<!-- 				<a class="book_now" href="https://www.eventbrite.com/e/work-awesome-tickets-21453749743">BOOK NOW</a> -->
+			</div>
+		</section>
+	</div>
+	<div id="container2" class="container">
+		<div class="body" >
+			<div class="articles col-sm-8">
+<!--	<section id="home">
 		<div class="description">
 			<img style="width:200px;margin:auto;" title="<?php echo bloginfo('name'); ?>" src="<?php echo get_template_directory_uri()."/img/work.png"?>">
 		</div>
-	</section>
+	</section>-->
